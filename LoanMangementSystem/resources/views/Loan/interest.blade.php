5e663402519a4ef5ab80bafddc69d054a6787c99
Ccaparas666 <carmelo.caparas@lccdo.edu.ph>
2024-01-29 02:17:58 +0800

    asd

--- LoanMangementSystem/resources/views/Loan/interest.blade.php
+++ LoanMangementSystem/resources/views/Loan/interest.blade.php
@@ -35,7 +35,7 @@
                                     <th scope="col" class="px-6 py-3">
                                         <div class="flex justify-center">{{ __('Interest Rate') }}</div>
                                     </th>
-                                    <th scope="col" class="px-6 py-3 flex justify-center">{{ __('ACTION') }}</th>   
+                                    <!-- <th scope="col" class="px-6 py-3 flex justify-center">{{ __('ACTION') }}</th>    -->
                                 </tr>
                                 
                             </thead>
@@ -44,7 +44,7 @@
                             @foreach ($loansettings as $settings)
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ">
                                 <td scope="row" class="text-center px-6 py-4 font-bold text-3xl text-gray-900 whitespace-nowrap dark:text-white ">{{ $settings->interest }} % </td>
-                                <td class="px-6 py-4 flex justify-center">
+                                <!-- <td class="px-6 py-4 flex justify-center">
                                         <a data-modal-target="Update" data-modal-toggle="Update"  data-tooltip-target="{{'tooltip-default-'. $settings->lsid}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm  px-3 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700" href= "#">
                                                 <svg class="w-[18px] h-[18px] text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                                                     <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z" />
@@ -98,7 +98,7 @@
                                                     }
                                                 </script>
                                             </form>
-                                        </td>
+                                        </td> -->
                             </tr>
                             
                             
